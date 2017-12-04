28fc128fca7e26268584504baf953ca149219b55
Akshay Thakre <akshay.thakre@mediciventures.com>
2017-12-03 22:08:26 -0700

    Initial commit

---
+++ springcloud-m1-startup/src/test/java/io/medici/springcloudm1startup/SpringcloudM1StartupApplicationTests.java
@@ -0,0 +1,16 @@
+package io.medici.springcloudm1startup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudM1StartupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
